00c9e5400e535278dd2669e2f09ed4b37649209a
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2017-07-14 11:08:35 +0200

    Ported plugin to pure scala.
    Change-Id: Ic15d871b6cd1ae383986f3221b0c2418fb5c20b5

--- src/main/scala/com/googlesource/gerrit/plugins/analytics/GsonFormatter.scala
+++ src/main/scala/com/googlesource/gerrit/plugins/analytics/GsonFormatter.scala
@@ -1,25 +1,34 @@
 // Copyright (C) 2016 The Android Open Source Project
+// Copyright (C) 2016 The Android Open Source Project
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
 // http://www.apache.org/licenses/LICENSE-2.0
-//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+package com.googlesource.gerrit.plugins.analytics
 
-package com.googlesource.gerrit.plugins.analytics;
+import java.io.PrintWriter
 
-import com.google.gerrit.sshd.PluginCommandModule;
+import com.google.gerrit.server.OutputFormat
+import com.google.gson.Gson
+import com.google.inject.Singleton
 
-public class SshModule extends PluginCommandModule {
+@Singleton class GsonFormatter {
+  val gsonBuilder = OutputFormat.JSON_COMPACT.newGsonBuilder
 
-  @Override
-  protected void configureCommands() {
-    command(ContributorsCommand.class);
+  def format[T](values: List[T], out: PrintWriter): Unit = {
+    val gson: Gson = gsonBuilder.create
+    values.foreach((value: T) => {
+      def foo(value: T) = {
+        gson.toJson(value, out)
+        out.println()
+      }
+      foo(value)
+    })
   }
 }
