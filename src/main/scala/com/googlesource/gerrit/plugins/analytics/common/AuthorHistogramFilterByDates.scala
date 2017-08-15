3c05129909b4e02432440a417b6fb52016737c74
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2017-08-15 02:27:54 +0200

    Add since/until limits to the contributors query
    Allow specifying since/until time-stamps for selecting a subset
    of commits grouped by contributor.
    
    Parameters are similar to Gerrit's time-stamp based query parameters.
    See: https://gerrit-review.googlesource.com/Documentation/user-search.html
    
    Change-Id: I47923130deca53c3a7ff86145597d43b704cbd5e

---
+++ src/main/scala/com/googlesource/gerrit/plugins/analytics/common/AuthorHistogramFilterByDates.scala
@@ -0,0 +1,41 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.analytics.common
+
+import java.util.Date
+
+import org.eclipse.jgit.revwalk.{RevCommit, RevWalk}
+import org.gitective.core.stat.CommitHistogramFilter
+
+/**
+  * Commit filter that includes commits only on the specified interval
+  * starting from and to excluded
+  */
+class AuthorHistogramFilterByDates(val from: Option[Long] = None, val to: Option[Long] = None)
+  extends CommitHistogramFilter {
+
+  override def include(walker: RevWalk, commit: RevCommit) = {
+    val commitDate = commit.getCommitterIdent.getWhen.getTime
+    val author = commit.getAuthorIdent
+    if (from.fold(true)(commitDate >=) && to.fold(true)(commitDate <)) {
+      histogram.include(commit, author)
+      true
+    } else {
+      false
+    }
+  }
+
+  override def clone = new AuthorHistogramFilterByDates(from, to)
+}
