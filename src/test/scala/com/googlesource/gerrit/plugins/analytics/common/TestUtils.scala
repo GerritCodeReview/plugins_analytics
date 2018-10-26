93c8b6d93efdf03cd6025167c6b89a489a6b8200
Antonio Barone <syntonyze@gmail.com>
2018-10-26 18:21:44 +0300

    Allow analytics to aggregate by branch
    When extractBranches is set, proceed to aggregate
    commits by branch name.
    
    Change-Id: Ib2d58f8dda397af476a7307e90dc7466a7edff2c

---
+++ src/test/scala/com/googlesource/gerrit/plugins/analytics/common/TestUtils.scala
@@ -0,0 +1,15 @@
+package com.googlesource.gerrit.plugins.analytics.common
+
+import com.googlesource.gerrit.plugins.analytics.test.GitTestCase
+import org.gitective.core.CommitFinder
+
+trait TestUtils {
+  self: GitTestCase =>
+
+  def aggregateBy(strategy: AggregationStrategy): Array[AggregatedUserCommitActivity] = {
+    val filter = new AggregatedHistogramFilterByDates(aggregationStrategy = strategy)
+    new CommitFinder(testRepo).setFilter(filter).find
+    filter.getHistogram.getAggregatedUserActivity
+  }
+
+}
