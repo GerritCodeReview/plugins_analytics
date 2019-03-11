d2acb1a0587c6d20eb21e82efa118580374130ed
Antonio Barone <syntonyze@gmail.com>
2019-03-11 18:23:10 +0000

    Flag and aggregate commits by BotLike-ness
    With this change it is possible to provide a new bot-like-regexps
    parameter which allows to pass a comma separated list of regexps to be
    used in recognizing bot-like commits.
    
    When *all* files touched by a commit match those regexps, then the commit
    is flagged as is_bot_like.
    
    Change-Id: I0f3af74dc223e9f12c9e19592c226d784eceb6f7

---
+++ src/test/scala/com/googlesource/gerrit/plugins/analytics/common/BotLikeExtractorImplSpec.scala
@@ -0,0 +1,66 @@
+// Copyright (C) 2019 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.analytics.common
+
+import com.googlesource.gerrit.plugins.analytics.test.GitTestCase
+import org.scalatest.{FlatSpec, Matchers}
+
+class BotLikeExtractorImplSpec extends FlatSpec with Matchers with GitTestCase {
+
+  behavior of "isBotLike"
+
+  it should "return true when all files match bot-like identifiers" in {
+    val extractor = new BotLikeExtractorImpl(List(""".+\.xml""", """.+\.bzl"""))
+
+    extractor.isBotLike(Set(
+      "tools/maven/gerrit-acceptance-framework_pom.xml",
+      "tools/maven/gerrit-extension-api_pom.xml",
+      "tools/maven/gerrit-plugin-api_pom.xml",
+      "tools/maven/gerrit-plugin-gwtui_pom.xml",
+      "tools/maven/gerrit-war_pom.xml",
+      "version.bzl"
+    )).shouldBe(true)
+
+  }
+
+  it should "return false when some of the files match bot-like identifiers" in {
+    val extractor = new BotLikeExtractorImpl(List(""".+\.xml""", """.+\.bzl"""))
+
+    extractor.isBotLike(Set(
+      "tools/maven/gerrit-extension-api_pom.xml",
+      "tools/maven/gerrit-plugin-api_pom.xml",
+      "tools/maven/gerrit-plugin-gwtui_pom.xml",
+      "tools/maven/gerrit-war_pom.xml",
+      "javatests/com/google/gerrit/acceptance/api/change/PrivateChangeIT.java",
+      "version.bzl"
+    )).shouldBe(false)
+
+  }
+
+  it should "return false when no bot-like identifiers have been provided" in {
+    val extractor = new BotLikeExtractorImpl(List.empty)
+
+    extractor.isBotLike(Set(
+      "tools/maven/gerrit-extension-api_pom.xml",
+      "tools/maven/gerrit-plugin-api_pom.xml",
+      "tools/maven/gerrit-plugin-gwtui_pom.xml",
+      "tools/maven/gerrit-war_pom.xml",
+      "javatests/com/google/gerrit/acceptance/api/change/PrivateChangeIT.java",
+      "version.bzl"
+    )).shouldBe(false)
+
+  }
+
+}
