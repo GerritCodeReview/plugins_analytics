e902ce1eef8e648494c55dae81623db989e73601
Antonio Barone <syntonyze@gmail.com>
2019-03-11 18:23:10 +0000

    Flag and aggregate commits by BotLike-ness
    With this change it is possible to provide a new bot-like-regexps
    parameter which allows to pass a comma separated list of regexps to be
    used in recognizing bot-like commits.
    
    When *all* files touched by a commit match those regexps, then the commit
    is flagged as is_bot_like.
    
    Change-Id: I0f3af74dc223e9f12c9e19592c226d784eceb6f7

--- src/test/scala/com/googlesource/gerrit/plugins/analytics/test/CommitInfoSpec.scala
+++ src/test/scala/com/googlesource/gerrit/plugins/analytics/test/CommitInfoSpec.scala
@@ -8,14 +8,14 @@ import org.scalatest.{FlatSpec, Matchers}
 class CommitInfoSpec extends FlatSpec with Matchers {
 
   "CommitInfo" should "be serialised as JSON correctly" in {
-    val commitInfo = CommitInfo(sha1 = "sha", date = 1000l, merge = false, files = newHashSet("file1", "file2"))
+    val commitInfo = CommitInfo(sha1 = "sha", date = 1000l, merge = false, botLike = false, files = newHashSet("file1", "file2"))
 
     val gsonBuilder = OutputFormat.JSON_COMPACT.newGsonBuilder
 
     val actual = gsonBuilder.create().toJson(commitInfo)
     List(actual) should contain oneOf(
-      "{\"sha1\":\"sha\",\"date\":1000,\"merge\":false,\"files\":[\"file1\",\"file2\"]}",
-      "{\"sha1\":\"sha\",\"date\":1000,\"merge\":false,\"files\":[\"file2\",\"file1\"]}"
+      "{\"sha1\":\"sha\",\"date\":1000,\"merge\":false,\"bot_like\":false,\"files\":[\"file1\",\"file2\"]}",
+      "{\"sha1\":\"sha\",\"date\":1000,\"merge\":false,\"bot_like\":false,\"files\":[\"file2\",\"file1\"]}"
     )
   }
 
