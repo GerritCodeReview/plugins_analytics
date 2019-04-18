de98d15b1800e0b04e01553a9e705d396989135f
Antonio Barone <syntonyze@gmail.com>
2019-04-18 16:36:53 +0300

    Improve the performance of branch extraction
    Improve the performance of contributors endpoint by using
    an in-memory/on-disk cache to avoid computing file diff for the same
    objectIds over and over again but for different branches.
    
    Remove also the use of parallel collection because of
    greatly diminishing the throughput caused by increasing the overhead
    in memory management.
    
    The performance improvement is higher for larget repositories with
    a lot of branches.
    
    Examples:
    
    - Extracting analytics, including branches, since 2000-01-01 from the
    Gerrit Code Review repo, which at the time of writing has a size of 130Mb
    and has only 15 branches might save up to 10 seconds. Arguably not that
    much.
    
    - Extracting analytics, including branches, since 2017-01-01 from the
    platform/prebuilts/tools repo (part of AOSP), which at the time of
    writing has a size of 22Gb and 215 branchea, might help latency to go
    down from 7minutes to 20 seconds (20 times faster).
    
    Extracting from this very same repo since 2000-01-01, allows to get a
    response in a matter of minutes rather several hours.
    
    Bug: Issue 10729
    Change-Id: I991a5fc82d7c32c6e035da8b90ba4bebeab50188

---
+++ src/main/scala/com/googlesource/gerrit/plugins/analytics/common/CommitsStatisticsCache.scala
@@ -0,0 +1,51 @@
+// Copyright (C) 2019 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.analytics.common
+
+import java.util.concurrent.Callable
+
+import com.google.common.cache.{Cache, LoadingCache}
+import com.google.gerrit.extensions.api.projects.CommentLinkInfo
+import com.google.gerrit.server.cache.serialize.{CacheSerializer, ObjectIdCacheSerializer}
+import com.google.inject.Inject
+import com.google.inject.name.Named
+import com.googlesource.gerrit.plugins.analytics.common.CommitsStatisticsCache.COMMITS_STATISTICS_CACHE
+import com.googlesource.gerrit.plugins.analytics.{CommitInfo, IssueInfo}
+import com.googlesource.gerrit.plugins.analytics.common.ManagedResource.use
+import org.eclipse.jgit.diff.{DiffFormatter, RawTextComparator}
+import org.eclipse.jgit.lib.{ObjectId, ObjectIdSerializer, Repository}
+import org.eclipse.jgit.revwalk.RevWalk
+import org.eclipse.jgit.treewalk.{CanonicalTreeParser, EmptyTreeIterator}
+import org.eclipse.jgit.util.io.DisabledOutputStream
+
+import scala.util.matching.Regex
+import scala.collection.JavaConverters._
+
+trait CommitsStatisticsCache {
+  def get(project: String, objectId: ObjectId): CommitsStatistics
+}
+
+case class CommitsStatisticsCacheKey(projectName: String, commitId: ObjectId)
+
+object CommitsStatisticsCache {
+  final val COMMITS_STATISTICS_CACHE = "commits_statistics_cache"
+}
+
+class CommitsStatisticsCacheImpl @Inject() (@Named(COMMITS_STATISTICS_CACHE) commitStatsCache: LoadingCache[CommitsStatisticsCacheKey, CommitsStatistics]
+) extends CommitsStatisticsCache {
+
+  override def get(project: String, objectId: ObjectId): CommitsStatistics =
+    commitStatsCache.get(CommitsStatisticsCacheKey(project, objectId))
+}
