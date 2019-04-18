93c385ecdf3acb5e5cfd3efdd4f6d35027715b2d
Antonio Barone <syntonyze@gmail.com>
2019-04-18 16:36:53 +0300

    Improve performance of branch extraction
    This change improves the performance of contributors endpoint by making
    use of an in-memory cache to avoid computing file diff for the same
    objectIds over and over again for different branches.
    
    Bug: Issue 10729
    Change-Id: I991a5fc82d7c32c6e035da8b90ba4bebeab50188

---
+++ src/main/scala/com/googlesource/gerrit/plugins/analytics/common/CacheApi.scala
@@ -0,0 +1,31 @@
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.analytics.common
+
+import org.eclipse.jgit.lib.ObjectId
+
+trait CacheApi[A, B] {
+  def getOrUpdate(el: A, f: => A => B): B
+}
+
+final class InMemoryCommitStatisticsCache extends CacheApi[ObjectId, CommitsStatistics] {
+
+  private val cache = collection.mutable.Map.empty[ObjectId, CommitsStatistics]
+
+  override def getOrUpdate(el: ObjectId, f: => ObjectId => CommitsStatistics): CommitsStatistics = cache.getOrElse(el, {
+    cache.update(el, f(el))
+    f(el)
+  })
+}
