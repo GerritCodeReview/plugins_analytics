6b61041ee19f1e1794f269b3165893fd9be3b500
Fabio Ponciroli <ponch78@gmail.com>
2025-01-14 16:22:19 +0100

    Expose starting ref of extraction in payload
    Add "starting_revision" field in payload. The field will
    be populated with the value of the "--branch" filter
    when specified or defaulted to HEAD.
    
    It can be useful for API consumers, when the client needs
    to track from which revision a specific extraction started from.
    
    Change-Id: I22369b86803c642ed5f1c9e7bfc57d1b62283e05

--- src/test/scala/com/googlesource/gerrit/plugins/analytics/test/UserActivitySummarySpec.scala
+++ src/test/scala/com/googlesource/gerrit/plugins/analytics/test/UserActivitySummarySpec.scala
@@ -5,8 +5,10 @@ import com.google.gerrit.entities.Project
 import com.googlesource.gerrit.plugins.analytics.UserActivitySummary
 import com.googlesource.gerrit.plugins.analytics.common.AggregationStrategy.EMAIL
 import com.googlesource.gerrit.plugins.analytics.common.{Statistics, TestUtils}
+import org.eclipse.jgit.internal.storage.file.FileRepository
 import org.eclipse.jgit.lib.Repository
 import org.scalatest.{FlatSpec, Matchers}
+import org.eclipse.jgit.lib.Constants.HEAD
 
 @UseLocalDisk
 class UserActivitySummarySpec extends FlatSpec with GerritTestDaemon with TestCommitStatisticsNoCache with TestUtils with Matchers {
@@ -26,13 +28,39 @@ class UserActivitySummarySpec extends FlatSpec with GerritTestDaemon with TestCo
     cloneRepo.push
 
     val aggregatedCommits = aggregateBy(EMAIL)(repository)
-    val summary = UserActivitySummary.apply(new Statistics(newProjectKey, commitsStatisticsNoCache))(aggregatedCommits.head)
+    val summary = UserActivitySummary.apply(new Statistics(newProjectKey, commitsStatisticsNoCache), HEAD)(aggregatedCommits.head)
 
     val nonMergeSummary = summary.head
     val mergeSummary = summary.drop(1).head
 
     nonMergeSummary.numCommits should be(2)
     mergeSummary.numCommits should be(1)
+  }
+
+  "UserActivitySummary" should "correctly set starting ref" in {
+    val (newProjectKey: Project.NameKey, repository: FileRepository) = createRepositoryWithCommit
+
+    val aggregatedCommits = aggregateBy(EMAIL)(repository)
+    val summaryHead = UserActivitySummary.apply(new Statistics(newProjectKey, commitsStatisticsNoCache), HEAD)(aggregatedCommits.head)
+    summaryHead.head.startingRevision shouldBe(HEAD)
+
+    val nonHead = "nonHeadRef"
+    val summaryNonHead = UserActivitySummary.apply(new Statistics(newProjectKey, commitsStatisticsNoCache), nonHead)(aggregatedCommits.head)
+    summaryNonHead.head.startingRevision shouldBe(nonHead)
+  }
+
+  private def createRepositoryWithCommit = {
+    val personEmail = "aCommitter@aCompany.com"
+
+    val newProjectKey: Project.NameKey = daemonTest.newProject(testSpecificRepositoryName, true)
+    val repository = daemonTest.getRepository(newProjectKey)
+    val cloneRepo = GitUtil.newTestRepository(repository).gitClone
+    getRepoOwnedByPerson(personEmail, repo = cloneRepo)
+
+    val personIdent = newPersonIdent("aPerson", personEmail)
+    cloneRepo.commitFile("aFile.txt", "some content", committer = personIdent, author = personIdent)
+    cloneRepo.push
+    (newProjectKey, repository)
   }
 
   def getRepoOwnedByPerson(email: String = author.getEmailAddress, repo: TestFileRepository = testFileRepository): Repository = {
